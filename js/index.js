ac70fed0d151998bdac0c92461213ffe8e64b047
David <digital0david@gmail.com>
2019-02-14 23:25:53 -0500

    nav js partially working

--- js/index.js
+++ js/index.js
@@ -6,16 +6,29 @@ let navBar = document.querySelector('.nav-bar');
 let navA = document.querySelector('.nav-bar a')
 let logo = document.querySelector('.logo');
 
-hamburger.addEventListener('click', () => {
-    hamburger.style.display = 'none';
-    hamburgerClose.style.display = 'unset';
 
-    navBar.style.height = '100%';
-    navBar.style.width = '1000px';
-    navBar.style.opacity = '.5';
-    navBar.style.opacity = '.75';
-    navBar.style.position = 'fixed';
-    navBar.style.zIndex = '1';
-    navBar.style.alignItems = 'flex-start';
-    navBar.style.paddingTop = '1%';
+
+
+hamburger.addEventListener('click', () => {
+    if (!navBar.classList.contains('nav-toggle')) {
+        navBar.classList.add('nav-toggle');
+        hamburger.style.display = 'none';
+        hamburgerClose.style.display = 'unset';
+    } else {
+        navBar.classList.remove('nav-toggle');
+        hamburger.style.display = 'unset';
+        hamburgerClose.style.display = 'none';
+    }
+});
+
+hamburgerClose.addEventListener('click', () => {
+    if (!navBar.classList.contains('nav-toggle')) {
+        navBar.classList.add('nav-toggle');
+        hamburger.style.display = 'none';
+        hamburgerClose.style.display = 'unset';
+    } else {
+        navBar.classList.remove('nav-toggle');
+        hamburger.style.display = 'unset';
+        hamburgerClose.style.display = 'none';
+    }
 });
