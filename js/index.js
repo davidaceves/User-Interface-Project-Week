26992bd423e830aa92ca28ae3d406fa05d700f9f
David <digital0david@gmail.com>
2019-02-19 21:09:47 -0500

    index.js complete

--- js/index.js
+++ js/index.js
@@ -27,3 +27,66 @@ hamburgerClose.addEventListener('click', () => {
         hamburgerClose.style.display = 'none';
     } 
 });
+
+class TabLink {
+    constructor(tabElement){
+      this.tabElement = tabElement;
+
+      this.tabData = this.tabElement.dataset.tab;
+      
+      this.itemElement = document.querySelector(`.tab-item-title[data-tab = "${this.tabData}"]`);
+
+      this.itemImg = document.querySelector(`.tab-item-img[data-tab = "${this.tabData}"]`);
+
+      this.tabTitle = new TabTitle(this.itemElement);
+      this.tabImg = new TabImg(this.itemImg);
+
+      console.log(this.tabTitle);
+    
+      this.tabElement.addEventListener('click', () => {
+        this.selectTab();
+      });
+    }
+
+    selectTab(){
+        const tabs = document.querySelectorAll('.tab');
+     
+        tabs.forEach(tab => tab.classList.remove('link-selected'));
+        
+        this.tabElement.classList.add('link-selected');
+
+        this.tabTitle.select();
+        this.tabImg.selectImg();
+      }
+    }
+
+    class TabTitle {
+        constructor(element) {
+          this.element = element;
+        }
+      
+        select() {
+          const items = document.querySelectorAll('.tab-item-title');
+      
+          Array.from(items).forEach(item => item.classList.remove('title-selected'));
+
+          this.element.classList.add('title-selected');
+        }
+      }
+
+      class TabImg {
+        constructor(element) {
+          this.element = element;
+        }
+      
+        selectImg() {
+          const imgItem = document.querySelectorAll('.tab-item-img');
+      
+          Array.from(imgItem).forEach(img => img.classList.remove('img-selected'));
+
+          this.element.classList.add('img-selected');
+        }
+      }
+
+let tabs = document.querySelectorAll(".tab")
+                   .forEach(tab => new TabLink(tab));
